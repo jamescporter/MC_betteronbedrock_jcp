60381e0158af799c98b0376d47a65056b518700c
James C Porter <jamescporter@hotmail.co.uk>
2026-03-10 17:33:41 +0000

    Optimize ambience owner validation tag checks

--- BOB_behaviors/scripts/functionality/ambience/ambient_stuff.js
+++ BOB_behaviors/scripts/functionality/ambience/ambient_stuff.js
@@ -1,4 +1,10 @@
 import { world, system, GameMode } from "@minecraft/server";
+
+const AMBIENT_UPDATE_DISTANCE_SQUARED = 9;
+
+const playerAmbientState = new Map();
+const ENTITY_ID_TAG_PATTERN = /^[0-9a-fA-F-]{36}$/;
+
 system.runInterval(() => {
     const players = world.getAllPlayers();
     for (let i = 0; i < players.length; i++) {
@@ -10,9 +16,9 @@ system.runInterval(() => {
             case "minecraft:the_end": {
                 player.playSound("ambient.random.end");
                 break;
-            };
-        };
-    };
+            }
+        }
+    }
 }, Math.random() * (880 - 480) + 480);
 
 system.runInterval(() => {
@@ -28,10 +34,10 @@ system.runInterval(() => {
             case "minecraft:overworld": {
                 player.playSound("confused_and_deeds_and_prelude");
                 break;
-            };
-        };
-    };
-}, 20 * 320)
+            }
+        }
+    }
+}, 20 * 320);
 
 function removeAmbienceTags(entity) {
     entity.removeTag("night");
@@ -55,124 +61,311 @@ function removeAmbienceTags(entity) {
     entity.removeTag("colds");
     entity.removeTag("desert");
     entity.removeTag("deserts");
-};
+}
 
-function removeAmbience(player) {
-    const { location, dimension, id: playerId } = player;
-    const ambienceEntities = dimension.getEntities({
-        type: "better_on_bedrock:ambiententity", location
+function getState(player) {
+    if (!playerAmbientState.has(player.id)) {
+        playerAmbientState.set(player.id, {
+            lastDimensionId: "",
+            lastPosition: undefined,
+            lastMusicState: "",
+            ambienceEntityId: undefined,
+            forceUpdate: true
+        });
+    }
+
+    return playerAmbientState.get(player.id);
+}
+
+function clearState(playerId) {
+    playerAmbientState.delete(playerId);
+}
+
+function getEntityById(entityId) {
+    if (!entityId)
+        return undefined;
+
+    try {
+        return world.getEntity(entityId);
+    } catch {
+        return undefined;
+    }
+}
+
+function removeTrackedAmbience(player) {
+    const state = playerAmbientState.get(player.id);
+    if (!state)
+        return;
+
+    const entity = getEntityById(state.ambienceEntityId);
+    if (entity)
+        system.run(() => entity.remove());
+
+    state.ambienceEntityId = undefined;
+}
+
+function hasValidPlayerOwnerTag(entity) {
+    const tags = entity.getTags();
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+        if (!ENTITY_ID_TAG_PATTERN.test(tag))
+            continue;
+
+        try {
+            const owner = world.getEntity(tag);
+            if (owner && owner.typeId === "minecraft:player")
+                return true;
+        } catch {
+        }
+    }
+
+    return false;
+}
+
+function cleanupDuplicateAmbienceEntities(player, keepEntityId) {
+    const ambienceEntities = player.dimension.getEntities({
+        type: "better_on_bedrock:ambiententity",
+        location: player.location,
+        maxDistance: 6
     });
 
     for (let j = 0; j < ambienceEntities.length; j++) {
         const entity = ambienceEntities[j];
-        if (entity.getTags().includes(playerId))
-            system.run(() => entity.remove());
+        if (!hasValidPlayerOwnerTag(entity)) {
+            entity.remove();
+            continue;
+        }
+
+        if (!entity.getTags().includes(player.id))
+            continue;
+
+        if (entity.id === keepEntityId)
+            continue;
+
+        entity.remove();
+    }
+}
+
+function ensurePlayerAmbienceEntity(player, state) {
+    let entity = getEntityById(state.ambienceEntityId);
+
+    if (!entity || entity.dimension.id !== player.dimension.id) {
+        const ambienceEntities = player.dimension.getEntities({
+            type: "better_on_bedrock:ambiententity",
+            location: player.location,
+            maxDistance: 8
+        });
+
+        for (let j = 0; j < ambienceEntities.length; j++) {
+            const candidate = ambienceEntities[j];
+            if (!candidate.getTags().includes(player.id))
+                continue;
+
+            entity = candidate;
+            break;
+        }
+
+        if (!entity) {
+            if (!player.isValid())
+                return undefined;
+
+            try {
+                entity = player.dimension.spawnEntity("better_on_bedrock:ambiententity", player.location);
+                entity.addTag(player.id);
+            } catch {
+                return undefined;
+            }
+        }
+
+        state.ambienceEntityId = entity.id;
+    }
+
+    cleanupDuplicateAmbienceEntities(player, entity.id);
+
+    return entity;
+}
+
+function getMusicStateAndApply(player, playerAmbienceEntity) {
+    if (player.location.y <= 60) {
+        player.removeTag("night");
+        return "underground";
+    }
+
+    if (world.getTimeOfDay() >= 12500 && world.getTimeOfDay() <= 23500)
+        return "night";
+
+    if (!playerAmbienceEntity)
+        return "none";
+
+    if (playerAmbienceEntity.hasTag("plains"))
+        return "plain";
+    if (playerAmbienceEntity.hasTag("forests"))
+        return "forest";
+    if (playerAmbienceEntity.hasTag("birchs"))
+        return "birch";
+    if (playerAmbienceEntity.hasTag("oceans"))
+        return "ocean";
+    if (playerAmbienceEntity.hasTag("cherrygroves"))
+        return "cherrygrove";
+    if (playerAmbienceEntity.hasTag("deserts"))
+        return "desert";
+    if (playerAmbienceEntity.hasTag("taigas"))
+        return "taiga";
+    if (playerAmbienceEntity.hasTag("swamps"))
+        return "swamp";
+    if (playerAmbienceEntity.hasTag("jungles"))
+        return "jungle";
+    if (playerAmbienceEntity.hasTag("colds"))
+        return "cold";
+
+    return "none";
+}
+
+function applyMusicTransition(player, playerAmbienceEntity, musicState) {
+    if (musicState === "underground") {
+        if (playerAmbienceEntity)
+            playerAmbienceEntity.triggerEvent("despawn");
+        return;
+    }
+
+    if (musicState === "night") {
+        if (playerAmbienceEntity)
+            playerAmbienceEntity.triggerEvent("despawn");
+
+        player.addTag("night");
+        player.playMusic("ambient.night", {
+            loop: true,
+            fade: 1.2,
+            volume: 0.1
+        });
+        return;
+    }
+
+    if (player.hasTag("night"))
+        player.removeTag("night");
+
+    if (musicState === "none")
+        return;
+
+    if (!playerAmbienceEntity)
+        return;
+
+    removeAmbienceTags(playerAmbienceEntity);
+    playerAmbienceEntity.addTag(musicState);
+
+    switch (musicState) {
+        case "plain":
+            player.playMusic("ambient.plains", { loop: true, fade: 1.2, volume: 0.4 });
+            break;
+        case "forest":
+            player.playMusic("ambient.bird", { loop: true, fade: 1.2, volume: 0.15 });
+            break;
+        case "birch":
+            player.playMusic("ambient.birch_bird", { loop: true, fade: 1.2, volume: 0.3 });
+            break;
+        case "ocean":
+            player.playMusic("ambient.ocean", { loop: true, fade: 1.2, volume: 1.2 });
+            break;
+        case "cherrygrove":
+            player.playMusic("ambient.cherry", { loop: true, fade: 1.2, volume: 0.7 });
+            break;
+        case "desert":
+            player.playMusic("ambient.desert", { loop: true, fade: 1.2, volume: 0.6 });
+            break;
+        case "taiga":
+        case "swamp":
+        case "jungle":
+            player.playMusic("ambient.taiga_bird", { loop: true, fade: 1.2, volume: 0.07 });
+            break;
+        case "cold":
+            player.playMusic("ambient.snow", { loop: true, fade: 1.2, volume: 0.3 });
+            break;
+    }
+}
+
+function shouldUpdateForMovement(state, player) {
+    if (state.forceUpdate)
+        return true;
+
+    if (state.lastDimensionId !== player.dimension.id)
+        return true;
+
+    if (!state.lastPosition)
+        return true;
+
+    const dx = player.location.x - state.lastPosition.x;
+    const dy = player.location.y - state.lastPosition.y;
+    const dz = player.location.z - state.lastPosition.z;
+
+    return (dx * dx + dy * dy + dz * dz) >= AMBIENT_UPDATE_DISTANCE_SQUARED;
+}
+
+function updatePlayerTrackingState(state, player, musicState) {
+    state.lastDimensionId = player.dimension.id;
+    state.lastPosition = {
+        x: player.location.x,
+        y: player.location.y,
+        z: player.location.z
     };
-};
+    state.lastMusicState = musicState;
+    state.forceUpdate = false;
+}
 
-world.beforeEvents.playerLeave.subscribe(
-    ({ player }) => removeAmbience(player),
-);
+world.beforeEvents.playerLeave.subscribe(({ playerId }) => {
+    clearState(playerId);
+});
 
-world.afterEvents.playerSpawn.subscribe(
-    ({ player }) => removeAmbience(player),
-);
+world.afterEvents.playerSpawn.subscribe(({ player, initialSpawn }) => {
+    const state = getState(player);
+    state.forceUpdate = true;
+    if (!initialSpawn)
+        removeTrackedAmbience(player);
+});
 
 world.afterEvents.playerDimensionChange.subscribe(({ player, toDimension }) => {
+    const state = getState(player);
+    state.forceUpdate = true;
+
     if (!player.hasTag("pog:ambientSounds"))
         return;
 
-    if(toDimension.id == 'minecraft:the_end' || toDimension.id == 'minecraft:nether') {
+    if (toDimension.id === "minecraft:the_end" || toDimension.id === "minecraft:nether") {
         player.stopMusic();
-
-        const playerId = player.id;
-        const ambienceEntities = toDimension.getEntities({ type: "better_on_bedrock:ambiententity"});
-        for (let j = 0; j < ambienceEntities.length; j++) {
-            const entity = ambienceEntities[j];
-            if (entity.getTags().includes(playerId))
-                entity.remove();
-        };
+        removeTrackedAmbience(player);
     }
-})
+});
 
 export function ambience(player) {
-    if (!player.hasTag("pog:ambientSounds")) {
-        removeAmbience(player);
-        removeAmbienceTags(player);
-        return;
-    };
+    const state = getState(player);
 
-    const ambienceEntities = player.dimension.getEntities({
-        type: "better_on_bedrock:ambiententity",
-        location: player.location
-    });
+    if (!player.hasTag("pog:ambientSounds")) {
+        removeTrackedAmbience(player);
+        removeAmbienceTags(player);
+        state.lastMusicState = "";
+        state.forceUpdate = true;
+        return;
+    }
+
+    if (!shouldUpdateForMovement(state, player))
+        return;
 
     let playerAmbienceEntity;
-    for (let j = 0; j < ambienceEntities.length; j++) {
-        const entity = ambienceEntities[j];
-        if (!entity.getTags().some((tag) => {
-            try {
-                return world.getEntity(tag) !== undefined
-            } catch {};
-        })) {
-            entity.remove();
-            continue;
-        };
-
-        if (entity.getTags().includes(player.id))
-            playerAmbienceEntity = entity;
-    };
 
     switch (player.dimension.id) {
         case "minecraft:the_end":
         case "minecraft:nether": {
-            if (player.hasTag("overworld"))
-                return;
-            player.addTag("overworld");
-            break;
-        };
+            if (!player.hasTag("overworld"))
+                player.addTag("overworld");
+
+            removeTrackedAmbience(player);
+            updatePlayerTrackingState(state, player, "other_dimension");
+            return;
+        }
         case "minecraft:overworld": {
+            playerAmbienceEntity = ensurePlayerAmbienceEntity(player, state);
 
-            if (player.location.y <= 60) {
-
-                if (playerAmbienceEntity !== undefined)
-                    playerAmbienceEntity.triggerEvent("despawn");
-
-                player.removeTag("night");
-                return;
-            };
-
-            if (world.getTimeOfDay() >= 12500 && world.getTimeOfDay() <= 23500) {
-                if (player.hasTag("night"))
-                    return;
-
-                if (playerAmbienceEntity !== undefined)
-                    playerAmbienceEntity.triggerEvent("despawn");
-
-                player.addTag("night");
-                player.playMusic("ambient.night", {
-                    loop: true,
-                    fade: 1.2,
-                    volume: 0.1
-                });
-            }
-            else {
-                if (player.hasTag("night"))
-                    player.removeTag("night");
-
-
-                if (playerAmbienceEntity == undefined) {
-                    if (!player.isValid())
-                        return;
-                    
-                    try {
-                        const entity = player.dimension.spawnEntity("better_on_bedrock:ambiententity", player.location);
-                        entity.addTag(player.id);
-                    } catch {};
-                    return;
-                };
-
-                const head = player.getHeadLocation()
+            if (playerAmbienceEntity) {
+                const head = player.getHeadLocation();
                 const view = player.getViewDirection();
 
                 playerAmbienceEntity.teleport({
@@ -180,140 +373,14 @@ export function ambience(player) {
                     y: head.y - view.y * 0.5,
                     z: head.z - view.z * 0.5
                 });
+            }
 
-                if (playerAmbienceEntity.hasTag("plains")) {
-                    if (playerAmbienceEntity.hasTag("plain"))
-                        return;
+            const musicState = getMusicStateAndApply(player, playerAmbienceEntity);
+            if (musicState !== state.lastMusicState)
+                applyMusicTransition(player, playerAmbienceEntity, musicState);
 
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("plain");
-                    
-                    player.playMusic("ambient.plains", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.4
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("forests")) {
-                    if (playerAmbienceEntity.hasTag("forest"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("forest");
-
-                    player.playMusic("ambient.bird", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.15
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("birchs")) {
-                    if (playerAmbienceEntity.hasTag("birch"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("birch");
-                    
-                    player.playMusic("ambient.birch_bird", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.3
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("oceans")) {
-                    if (playerAmbienceEntity.hasTag("ocean"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("ocean");
-                    
-                    player.playMusic("ambient.ocean", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 1.2
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("cherrygroves")) {
-                    if (playerAmbienceEntity.hasTag("cherrygrove"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("cherrygrove");
-                    
-                    player.playMusic("ambient.cherry", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.7
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("deserts")) {
-                    if (playerAmbienceEntity.hasTag("desert"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("desert");
-                    
-                    player.playMusic("ambient.desert", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.6
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("taigas")) {
-                    if (playerAmbienceEntity.hasTag("taiga"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("taiga");
-
-                    player.playMusic("ambient.taiga_bird", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.07
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("swamps")) {
-                    if (playerAmbienceEntity.hasTag("swamp"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("swamp");
-                    
-                    player.playMusic("ambient.taiga_bird", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.07
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("jungles")) {
-                    if (playerAmbienceEntity.hasTag("jungle"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("jungle");
-                    
-                    player.playMusic("ambient.taiga_bird", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.07
-                    });
-                }
-                if (playerAmbienceEntity.hasTag("colds")) {
-                    if (playerAmbienceEntity.hasTag("cold"))
-                        return;
-
-                    removeAmbienceTags(playerAmbienceEntity);
-                    playerAmbienceEntity.addTag("cold");
-                    
-                    player.playMusic("ambient.snow", {
-                        loop: true,
-                        fade: 1.2,
-                        volume: 0.3
-                    });
-                }
-               
-            };
-            break;
-        };
-    };
-};
+            updatePlayerTrackingState(state, player, musicState);
+            return;
+        }
+    }
+}
--- BOB_behaviors/scripts/functionality/ambience/fog.js
+++ BOB_behaviors/scripts/functionality/ambience/fog.js
@@ -1,75 +1,185 @@
-import { world, system, Player, MinecraftDimensionTypes } from "@minecraft/server";
+import { world, system, MinecraftDimensionTypes } from "@minecraft/server";
 
-const END_FOG_TAGS = [
-    "void",
-    "vacant",
-    "chorus",
-    "abyssal",
-    "wastes",
-    "shroom"
-];
+const END_UPDATE_DISTANCE_SQUARED = 9;
 
-/**
- * Replaces the fog tag on a player.
- * @param {Player} player The player to set the fog tag on.
- * @param {string} tag The unique fog tag to give the player.
- */
-function SetFogTag(player, tag) {
-    for (const fogTag of END_FOG_TAGS)
-        player.removeTag(fogTag);
-    player.runCommand("fog @p remove bob:end_fog")
-    player.addTag(tag);
-}
+const END_BIOME_BY_BLOCK = {
+    "better_on_bedrock:shrub_nylium": {
+        fog: "pog:vacant_dusk",
+        music: "ambient.vacant"
+    },
+    "better_on_bedrock:void_grass": {
+        fog: "pog:voiding_plains",
+        music: "ambient.voiding_plains"
+    },
+    "better_on_bedrock:shroom_nylium": {
+        fog: "pog:fungal_grove",
+        music: "ambient.fungal_grove"
+    },
+    "better_on_bedrock:fall_nylium": {
+        fog: "pog:voided_abyssal",
+        music: "ambient.voided_abyss"
+    },
+    "better_on_bedrock:chorus_grass": {
+        fog: "pog:chorus_forest",
+        music: "ambient.chorus_forest"
+    },
+    "minecraft:end_stone": {
+        fog: "",
+        music: "ambient.end_wastes"
+    }
+};
 
+const playerEndState = new Map();
 const theEnd = world.getDimension(MinecraftDimensionTypes.theEnd);
 
+function getState(player) {
+    if (!playerEndState.has(player.id)) {
+        playerEndState.set(player.id, {
+            dimensionId: "",
+            position: undefined,
+            biomeKey: "",
+            fogKey: "",
+            musicKey: "",
+            forceUpdate: true
+        });
+    }
+
+    return playerEndState.get(player.id);
+}
+
+function clearState(playerId) {
+    playerEndState.delete(playerId);
+}
+
+function shouldProcess(player, state) {
+    if (state.forceUpdate)
+        return true;
+
+    if (state.dimensionId !== player.dimension.id)
+        return true;
+
+    if (!state.position)
+        return true;
+
+    const dx = player.location.x - state.position.x;
+    const dy = player.location.y - state.position.y;
+    const dz = player.location.z - state.position.z;
+
+    return (dx * dx + dy * dy + dz * dz) >= END_UPDATE_DISTANCE_SQUARED;
+}
+
+function cachePositionAndDimension(player, state) {
+    state.dimensionId = player.dimension.id;
+    state.position = {
+        x: player.location.x,
+        y: player.location.y,
+        z: player.location.z
+    };
+    state.forceUpdate = false;
+}
+
+function removeFogIfActive(player, state, force = false) {
+    if (!state.fogKey && !force)
+        return;
+
+    player.runCommand("fog @s remove bob:end_fog");
+    state.fogKey = "";
+}
+
+function applyEndBiomeTransition(player, state, biomeState) {
+    const nextFog = biomeState?.fog ?? "";
+    const nextMusic = biomeState?.music ?? "";
+
+    if (state.fogKey !== nextFog) {
+        if (state.fogKey)
+            player.runCommand("fog @s remove bob:end_fog");
+
+        if (nextFog)
+            player.runCommand(`fog @s push ${nextFog} bob:end_fog`);
+
+        state.fogKey = nextFog;
+    }
+
+    if (state.musicKey !== nextMusic) {
+        if (nextMusic)
+            player.playMusic(nextMusic, { volume: 1, fade: 1, loop: true });
+
+        state.musicKey = nextMusic;
+    }
+}
+
+function getBiomeFromFloor(player) {
+    const floorBlock = theEnd.getBlockFromRay(
+        { x: player.location.x, y: player.location.y + 1, z: player.location.z },
+        { x: 0, y: -1, z: 0 }
+    )?.block;
+
+    if (!floorBlock)
+        return "";
+
+    return floorBlock.typeId;
+}
+
+function processPlayerEndFog(player) {
+    const state = getState(player);
+    if (!shouldProcess(player, state))
+        return;
+
+    if (player.dimension.id !== "minecraft:the_end" || !player.hasTag("endFog")) {
+        removeFogIfActive(player, state, state.forceUpdate);
+        state.biomeKey = "";
+        state.musicKey = "";
+        cachePositionAndDimension(player, state);
+        return;
+    }
+
+    const biomeKey = getBiomeFromFloor(player);
+    if (biomeKey === state.biomeKey && !state.forceUpdate) {
+        cachePositionAndDimension(player, state);
+        return;
+    }
+
+    applyEndBiomeTransition(player, state, END_BIOME_BY_BLOCK[biomeKey]);
+    state.biomeKey = biomeKey;
+    cachePositionAndDimension(player, state);
+}
+
+world.beforeEvents.playerLeave.subscribe(({ playerId }) => {
+    clearState(playerId);
+});
+
+world.afterEvents.playerSpawn.subscribe(({ player }) => {
+    getState(player).forceUpdate = true;
+});
+
+world.afterEvents.playerDimensionChange.subscribe(({ player }) => {
+    const state = getState(player);
+    state.forceUpdate = true;
+
+    if (player.dimension.id !== "minecraft:the_end") {
+        removeFogIfActive(player, state, true);
+        state.biomeKey = "";
+        state.musicKey = "";
+    }
+});
+
 system.runInterval(() => {
-    system.runJob(function* () {   
-        for (const player of theEnd.getPlayers({tags: ["endFog"]})) {
-            const floorBlock = theEnd.getBlockFromRay({x: player.location.x, y: player.location.y + 1, z: player.location.z}, {x: 0, y: -1, z: 0})?.block;
-            if (floorBlock === undefined) continue;
-            if (floorBlock.typeId == 'better_on_bedrock:shrub_nylium' && !player.hasTag('vacant')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.runCommand("fog @p push pog:vacant_dusk bob:end_fog")
-                player.playMusic("ambient.vacant", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "vacant");
-            }
-            if (floorBlock.typeId == 'better_on_bedrock:void_grass' && !player.hasTag('void')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.runCommand("fog @p push pog:voiding_plains bob:end_fog")
-                player.playMusic("ambient.voiding_plains", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "void");
-            }
-            if (floorBlock.typeId == 'better_on_bedrock:shroom_nylium' && !player.hasTag('shroom')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.runCommand("fog @p push pog:fungal_grove bob:end_fog")
-                player.playMusic("ambient.fungal_grove", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "shroom");
-            }
-            if (floorBlock.typeId == 'better_on_bedrock:fall_nylium' && !player.hasTag('abyssal')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.runCommand("fog @p push pog:voided_abyssal bob:end_fog")
-                player.playMusic("ambient.voided_abyss", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "abyssal");
-            }
-            if (floorBlock.typeId == 'better_on_bedrock:chorus_grass' && !player.hasTag('chorus')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.runCommand("fog @p push pog:chorus_forest bob:end_fog")
-                player.playMusic("ambient.chorus_forest", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "chorus");
-            }
-            if (floorBlock.typeId == 'minecraft:end_stone' && !player.hasTag('wastes')) {
-                player.runCommand("fog @p remove bob:end_fog")
-                player.playMusic("ambient.end_wastes", { volume: 1, fade: 1, loop: true })
-                //SetFogTag(player, "wastes");
-            };
-        };
+    system.runJob(function* () {
+        for (const player of theEnd.getPlayers({ tags: ["endFog"] }))
+            processPlayerEndFog(player);
+
         const players = world.getAllPlayers();
-        for (let i = 0; i < players.length; i++) {
-            const player = players[i];
-           if(player.dimension.id != "minecraft:the_end" || !player.hasTag("endFog")) {
-                player.runCommand("fog @p remove bob:end_fog")
-           }
+        const activePlayersById = new Map(players.map((player) => [player.id, player]));
+
+        for (const [playerId] of playerEndState) {
+            const player = activePlayersById.get(playerId);
+            if (!player) {
+                playerEndState.delete(playerId);
+                continue;
+            }
+
+            if (player.dimension.id !== "minecraft:the_end" || !player.hasTag("endFog"))
+                processPlayerEndFog(player);
         }
     }());
 }, 30);
