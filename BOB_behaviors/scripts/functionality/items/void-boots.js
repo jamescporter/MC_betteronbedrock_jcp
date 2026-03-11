5d16add459db6743b77cc1c48f597cec9a86dc9c
James C Porter <jamescporter@hotmail.co.uk>
2026-03-11 00:04:16 +0000

    Reduce void boots cache-cleanup overhead

--- BOB_behaviors/scripts/functionality/items/void-boots.js
+++ BOB_behaviors/scripts/functionality/items/void-boots.js
@@ -1,5 +1,7 @@
 import { world, system, TicksPerSecond, EquipmentSlot, EntityEquippableComponent, ItemDurabilityComponent } from "@minecraft/server";
 
+const lastFillRegionByPlayer = new Map();
+
 function landCheck(player) {
     const blockUnder = player.dimension.getBlockFromRay(player.location, { x: 0, y: -1, z: 0 })?.block;
     const block1 = player.dimension.getBlockFromRay({ x: player.location.x - 2, y: player.location.y, z: player.location.z - 2 }, ({ x: 0, y: -1, z: 0 }))?.block;
@@ -17,22 +19,43 @@ export function voidBoots(player) {
     if (!player?.isValid())
         return;
 
-    if (player?.dimension?.id !== "minecraft:the_end")
+    if (player?.dimension?.id !== "minecraft:the_end") {
+        lastFillRegionByPlayer.delete(player.id);
         return;
+    }
 
     const equipment = player.getComponent(EntityEquippableComponent.componentId);
     const boots = equipment.getEquipment(EquipmentSlot.Feet);
-    if (boots?.typeId !== "better_on_bedrock:voiding_boots" || landCheck(player))
+    if (boots?.typeId !== "better_on_bedrock:voiding_boots" || landCheck(player)) {
+        lastFillRegionByPlayer.delete(player.id);
         return;
+    }
 
     const { x, y, z } = player.location;
+    const fillRegion = `${x - 1} ${y - 1} ${z - 1} ${x + 1} ${y - 1} ${z + 1}`;
     player.addEffect("slowness", TicksPerSecond * 3, { showParticles: false });
-    player.dimension.runCommandAsync(`fill ${x - 1} ${y - 1} ${z - 1} ${x + 1} ${y - 1} ${z + 1} better_on_bedrock:void_block [] replace air`);
+
+    if (lastFillRegionByPlayer.get(player.id) === fillRegion)
+        return;
+
+    lastFillRegionByPlayer.set(player.id, fillRegion);
+    player.dimension.runCommandAsync(`fill ${fillRegion} better_on_bedrock:void_block [] replace air`);
 };
 
 system.runInterval(() => {
     system.runJob(function* () {
         const players = world.getAllPlayers();
+        if (lastFillRegionByPlayer.size > 0) {
+            const activePlayerIds = new Set();
+            for (let i = 0; i < players.length; i++)
+                activePlayerIds.add(players[i].id);
+
+            for (const playerId of lastFillRegionByPlayer.keys()) {
+                if (!activePlayerIds.has(playerId))
+                    lastFillRegionByPlayer.delete(playerId);
+            }
+        }
+
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
             if (player.dimension.id !== "minecraft:the_end")
