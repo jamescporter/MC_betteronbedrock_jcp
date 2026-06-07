ca8967a46e2686fe1389f3bd741de459228d7b93
James Porter <jamescporter@hotmail.co.uk>
2026-06-07 17:21:26 +0100

    removed old json code

--- old_json/BOB_behaviors/scripts/functionality/index.js
+++
@@ -1,5 +0,0 @@
-import "./armor_effects/stardust.js";
-import "./items/backpacks.js";
-import "./quests/main.js";
-import "./ambience/ambient_stuff.js";
-import "./ambience/fog.js";
--- old_json/BOB_behaviors/scripts/scripting_events/after_events/item-use-on.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-
-import endBlosson from "../../functionality/blocks/end_blosson";
-
-world.afterEvents.itemUseOn.subscribe(
-    ({ block, itemStack, blockFace }) => {
-        endBlosson(block, itemStack, blockFace);
-    },
-);
